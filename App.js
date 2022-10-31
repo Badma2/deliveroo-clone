f357d0d19c40c64c61f34c278c8ce24400bcdf8e
Badma2 <112466412+Badma2@users.noreply.github.com>
2022-10-31 16:26:30 +0800

    update

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <TailwindProvider>
+        <View style={styles.container}>
+          <Text>Opn up pp.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </TailwindProvider>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
