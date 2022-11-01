34c5ff5f33aca23c2397f9d6233392ae34e49ef6
Badma2 <112466412+Badma2@users.noreply.github.com>
2022-11-01 12:52:23 +0800

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,11 @@
-import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native';
+import { 
+  View, 
+  Text, 
+  SafeAreaView, 
+  Image, 
+  TextInput, 
+  ScrollView 
+} from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import {
@@ -20,15 +27,14 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt-5">
-      <Text className="text-red-500">
-
+      <Text>
         {/* Header */}
         <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
           <Image 
             source={{
               uri: 'https://links.papareact.com/wru'
             }}
-            className='h-7 w-7 bg-slate-300 p-4 rounded-full'
+            className='h-7 w-7 bg-gray-300 p-4 rounded-full'
           />
 
           <View className="flex-1">
@@ -56,9 +62,8 @@ const HomeScreen = () => {
 
           <AdjustmentsIcon color="#00CCBB" />
         </View>
-
-        {/* Body */}
-        <ScrollView 
+                {/* Body */}
+                <ScrollView 
           className="bg-gray-100"
           contentContainerStyle={{
             paddingBottom: 100,
