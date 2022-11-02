628f86a18e45712924b5f7d78e9623eb24589232
Badma2 <badmaarag1025@gmail.com>
2022-11-02 22:32:43 +0800

    update

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -14,7 +14,7 @@ const RestaurantCard = ({
   lat,
 }) => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity className="bg-white mr-3 shadow">
       <Image
         source={{
           uri: imgUrl,
@@ -23,9 +23,23 @@ const RestaurantCard = ({
       />
 
       <View className="px-3 pb-4">
-        <FontAwesome5 name="arrow-down" size={20} color="#00CCBB" />
-
         <Text className="font-bold text-lg pt-2">{title}</Text>
+        <View className="flex-row items-center space-x-1">
+          <FontAwesome5 name="star" color="green" opacity={0.5} size={22} />
+          <Text className="text-xs text-gray-500">
+            <Text className="text-green-500">{rating}</Text> {genre}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center space-x-1">
+          <FontAwesome5
+            name="search-location"
+            color="gray"
+            opacity={0.4}
+            size={22}
+          />
+          <Text className="text-xs text-gray-500">Nearby {address}</Text>
+        </View>
       </View>
     </TouchableOpacity>
   );
--- components/categories.js
+++ components/categories.js
@@ -13,27 +13,27 @@ const Categories = () => {
       showsHorizontalScrollIndicator={false}
     >
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://links.papareact.com/gn7"
         title="Testing 1"
       />
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://links.papareact.com/gn7"
         title="Testing 2"
       />
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://links.papareact.com/gn7"
         title="Testing 3"
       />
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://links.papareact.com/gn7"
         title="Testing 3"
       />
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://links.papareact.com/gn7"
         title="Testing 3"
       />
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://links.papareact.com/gn7"
         title="Testing 3"
       />
     </ScrollView>
